19e1e79012dbb956d3689c259068d4b2766d1773
EnriqueSantos-dev <santosenrique869@gmail.com>
2022-01-10 22:33:24 -0300

    modulo git atualização final

--- js/js-script.js
+++
@@ -1,8 +0,0 @@
-var botao_menu = document.getElementById('botao')
-        botao_menu.addEventListener('click', toggle_botao)
-
-
-function toggle_botao() {
-    var nav = document.getElementById('navegacao')
-        nav.classList.toggle('active');
-}
